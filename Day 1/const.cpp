2893e3fda99490b05080f81ea85b4d5b1de75fac
Satish Mane <manes1777@gmail.com>
2023-04-14 20:55:50 +0530

    Create const.cpp

---
+++ Day 1/const.cpp
@@ -0,0 +1,27 @@
+// C++ program to demonstrate the use of default constructor
+
+#include <iostream>
+using namespace std;
+
+class  Wall {
+
+  private:
+    double length;
+
+  public:
+    // default constructor to initialize variable
+
+    Wall() {
+      length = 5.5;
+      cout << "Creating a wall." << endl;
+      cout << "Length = " << length << endl;
+
+    }
+    
+};
+
+
+int main() {
+  Wall wall1;
+  return 0;
+}
