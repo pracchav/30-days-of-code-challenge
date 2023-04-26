f12e1269a55674b6cb4ef7f9482233543e003d10
Satish Mane <manes1777@gmail.com>
2023-04-26 13:12:17 +0530

    Create variables.py

---
+++ Day 2/variables.py
@@ -0,0 +1,6 @@
+# its a container of a values behave as the value of that it contain
+#name = "Satish Mane"
+first_name = "Satish"
+last_name = "Mane"
+full_name = first_name + " " + last_name
+print("Hello " +full_name)
