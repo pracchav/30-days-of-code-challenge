c559b12c42916ee72cb65f8003377bb9bad91839
Satish Mane <manes1777@gmail.com>
2023-04-27 19:18:17 +0530

    Create typecasting.py

---
+++ Day 3/typecasting.py
@@ -0,0 +1,11 @@
+#tyoe casting this can change any data type to any another data type
+a = 1   #int
+b = 2.1 #float
+c = "3" #string
+
+a = str(a)  #this can change int to string
+c = int(c) #this can change string into the int
+
+print(a)
+print(b)
+print(c)
