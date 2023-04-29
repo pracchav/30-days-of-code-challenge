a13d2e05714e177bb879205c5ec6b436ba94562d
Satish Mane <manes1777@gmail.com>
2023-04-29 09:10:02 +0530

    Create showInfo.py

---
+++ Day 5/showInfo.py
@@ -0,0 +1,6 @@
+from tkinter import *
+from tkinter import messagebox
+top = Tk()
+top.geometry("1000x1000")
+messagebox.showinfo("Information","You  PC is Under Controll\n Sam The Hacker has been Hacked Your PC\n What you Have to do")
+top.mainloop()
