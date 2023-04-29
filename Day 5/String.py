40d9dbffcff175eea5c126a533fb67e00b962686
Satish Mane <manes1777@gmail.com>
2023-04-29 09:10:07 +0530

    Create String.py

---
+++ Day 5/String.py
@@ -0,0 +1,13 @@
+'''\n is use to get new line'''
+
+
+pharase = "This s cs50"
+
+
+print(pharase + " is cool")
+print(pharase[0])
+print(len(pharase))s
+print(pharase[3])
+print(pharase.index("cs"))
+'''replace any word in striings'''
+print(pharase.replace("cs50","Sam"))
