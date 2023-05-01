185cd7325eed7b3bfa7da3dfd93c5fe7c781afae
Satish Mane <manes1777@gmail.com>
2023-05-01 16:10:08 +0530

    Create number.py

---
+++ Day 7/number.py
@@ -0,0 +1,18 @@
+print(3.5+9)
+print(3*4+5)
+
+'''moduloas'''
+print(10%3)
+
+'''variable container'''
+my_num = -5
+print(my_num)
+
+'''convert number into string'''
+print(str(my_num)+ " my favorate nuumber")
+
+'''absolute values of a number'''
+print(abs(my_num))
+
+'''two picesog information'''
+print(pow(4,5))
