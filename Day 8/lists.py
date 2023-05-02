857e7848b77b918b3773960e8b4aa93ceef482c3
Satish Mane <manes1777@gmail.com>
2023-05-02 15:40:32 +0530

    Create lists.py

---
+++ Day 8/lists.py
@@ -0,0 +1,8 @@
+friends = ["Sanket", "Sam", "Prasad", "Rushi", "Aftab"]
+'''use indexx to accecc of the list name'''
+print(friends[3])
+''''reverse manner'''
+print(friends[1:3])
+'''modify elements'''
+friends[1] = "Mike"
+print(friends[1])
