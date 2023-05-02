13a9240130f11d9748793ac29d2ff82ac0662a51
Satish Mane <manes1777@gmail.com>
2023-05-02 15:40:37 +0530

    Create mad_game.py

---
+++ Day 8/mad_game.py
@@ -0,0 +1,13 @@
+
+color =input("Enter a color: ")
+plural = input("Enter A pplural: ")
+me = input("Enter A World: ")
+
+print("roses are Red")
+print("Vilets are Blue")
+print("I Love You")
+
+
+print("roses are " +color)
+print(plural+ " are Blue")
+print("I Love " + me )
