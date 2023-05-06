9ce3268cf49921a7b952a3697a4aa18b73316d34
Satish Mane <manes1777@gmail.com>
2023-05-06 13:56:16 +0530

    Create dictionary.py

---
+++ Day 11/dictionary.py
@@ -0,0 +1,17 @@
+    monthConversion = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "jun": "june",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Now": "Novombar",
+    "Dec": "Decembar"
+}
+
+print(monthConversion["Jan"])
+print(monthConversion.get("starwar3","Not a Valid Key"))
