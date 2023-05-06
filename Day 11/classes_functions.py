964873b7197ec93f736a49656eae0618ab29996e
Satish Mane <manes1777@gmail.com>
2023-05-06 13:56:12 +0530

    Create classes_functions.py

---
+++ Day 11/classes_functions.py
@@ -0,0 +1,12 @@
+class Myclass:
+    def getdata(self):
+        a = int (input ("Enter a Number"))
+        b = int (input("Enter 2nd Number"))
+    def addop(self):
+        c=a+b
+    def showdata(self):
+        print("result : ",c)
+obj1=Myclass()
+obj1=getdata|()
+obj1.addop()
+obj1=showdata()
