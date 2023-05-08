48cb1b97db612f7a092c132a6b8e11b6dde32b5a
Satish Mane <manes1777@gmail.com>
2023-05-08 08:08:27 +0530

    Create function.cpp

---
+++ Day 13/function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void sayHi(){
+    
+    cout << " Hello Users";
+}
+
+int main(){
+
+    cout << "Top " <<endl;
+
+    sayHi();
+
+
+
+    cout << " Buttom";
+}
