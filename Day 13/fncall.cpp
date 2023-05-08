fc68c375ba20daf80dfe0213f5fcf3b10c33493b
Satish Mane <manes1777@gmail.com>
2023-05-08 08:08:23 +0530

    Create fncall.cpp

---
+++ Day 13/fncall.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int DemoConsoleOutput();
+//this is the simple declaration of the function
+int main(){
+    DemoConsoleOutput();
+}
+int DemoConsoleOutput(){
+    cout << "This is a Simple string literal" << endl;
+    cout << "Writing Number : " << 5 << endl;
+    cout << "Perform Simple Division 10 / 5 = " << 10 / 5 << endl;
+    cout << " pi is 32 / 7 = " << 22.0 / 7 << endl;
+
+}
