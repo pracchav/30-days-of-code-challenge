9ed0c20937283ca08f7e0b2f024e1ba1c636f32d
Satish Mane <manes1777@gmail.com>
2023-05-10 16:39:15 +0530

    Create positivech.cpp

---
+++ Day 14/positivech.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+    
+    cout << "Enter a value: ";
+    cin >> a;
+    
+    // cout << " Value of a is " << a << endl; // this is te imple value printing statements.
+
+    if(a>0){
+        cout << " A is positive " << endl;
+    }
+    else{
+        cout << " A is Negative " << endl;
+    }
+}
