5958bd016ed6a993c0ec12f07f02d8454727c170
Satish Mane <manes1777@gmail.com>
2023-05-22 20:24:11 +0530

    Create cal.cpp

---
+++ Day 20/cal.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    int num1, num2;
+
+    cout << " Enter first number: ";
+    cin >> num1;
+
+    cout << " Enter an Another number: ";
+    cin >> num2;
+
+    cout << num1+num2;
+
+}
