d73e218984e61ff0d5a35c4d134bb5ebdcf69795
Satish Mane <manes1777@gmail.com>
2023-05-24 21:12:46 +0530

    Create math.cpp

---
+++ Day 21/math.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    cout << pow(2, 10) << endl; // this is the power function.
+
+    cout << sqrt(25) << endl; // this can be give us square root of given number.
+
+    cout << round(8.6) << endl; // this can be give us a rounded value (an integer value).
+
+    cout << fmax(10, 5) << endl;// find maximum in two numbers.
+
+    cout << fmin(126, 912) << endl;// find minimum in two numbers.
+
+}
