b963038680c7698fabc0ddc11942d46123914ec3
Satish Mane <manes1777@gmail.com>
2023-05-25 11:05:17 +0530

    Create multiplay.cpp

---
+++ Day 21/multiplay.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,mul;
+    cout<<"Enter the 1st number\n";
+    cin>>a;
+    cout<<"Enter the 2nd number\n";
+    cin>>b;
+    mul=a*b;
+    cout<<"Multiplication of two number "<<mul;
+    return 0;
+}
