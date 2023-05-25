b37bbcd0d32176255d9bab82fc51791f4c85a088
Satish Mane <manes1777@gmail.com>
2023-05-25 11:05:34 +0530

    Create reverenum.cpp

---
+++ Day 21/reverenum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Number: ";
+    cin>>n;
+    int reverse=0;
+    while(n>0){
+        int lastDigit = n%10;
+        reverse = reverse *10 + lastDigit;
+        n=n/10;
+    }
+    cout<<"reverse num: " <<reverse<<endl;
+}
