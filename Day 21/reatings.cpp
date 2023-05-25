f7fedd5f78856203bfe9593903db6fed38faf942
Satish Mane <manes1777@gmail.com>
2023-05-25 11:05:23 +0530

    Create reatings.cpp

---
+++ Day 21/reatings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "Dear Satish,\n";
+  std::cout << "      This is the great cource which you have just trarted and good luck for your next journey with the 30 Days of the Code.\n";
+  std::cout << "    Everything will be ok my  friends so mine yourself get get ready to be a Diamond and get ready so shine like a sun.\n";
+  
+  
+  
+  
+  
+}
