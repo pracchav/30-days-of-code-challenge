b35d4fe6bf0355e07fa2adb1b99755758b378295
Satish Mane <manes1777@gmail.com>
2023-05-26 06:36:30 +0530

    Create sample.cpp

---
+++ Day 22/sample.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num,num1;
+    int c;
+    cout << " Enter A user to perform an operation: ";
+    cin >> num;
+
+    cout << "Enter Another Number: ";
+    cin >> num1;
+
+    c= num+num1;
+    cout << c;
+
+}
