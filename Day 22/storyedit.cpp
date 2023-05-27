118dd2831e489188ff843379de0fcdb1e322870c
Satish Mane <manes1777@gmail.com>
2023-05-28 03:17:11 +0530

    Create storyedit.cpp

---
+++ Day 22/storyedit.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string charname;
+    int charage;
+    cout<< " Enter the character name: ";
+    cin>> charname;
+    cout<< "Enter characters age: ";
+    cin>> charage;
+    cout<< "There once was a man named "<< charname << endl;
+    cout<< "He was " << charage <<" Years old" << endl;
+    cout<< "He liked the name " << charname << endl;
+    cout<< "But he did not like being " << charage << endl;
+} 
