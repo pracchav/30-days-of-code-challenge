a06d536de2391621a4857a217a9ea1caa7dcb61c
Satish Mane <manes1777@gmail.com>
2023-05-29 08:21:46 +0530

    Create void.cpp

---
+++ Day 23/void.cpp
@@ -0,0 +1,9 @@
+// CPP Program to demonstrate main() with
+// return type
+#include <iostream>
+using namespace std;
+// Driver Code
+int main()
+{
+    cout<<"This program returns the integer value 0\n";
+}
