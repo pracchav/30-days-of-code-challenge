b00f2ac1083224ac610d0bdde9615d6dbe23df51
Satish Mane <manes1777@gmail.com>
2023-05-29 08:22:03 +0530

    Create workwithnum.cpp

---
+++ Day 23/workwithnum.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << 40 <<endl;
+
+    //this is also a method to do the calculation and all stuff
+
+        int wnum = 5;
+        int num = 14;
+        int num1 = 125;
+        double dnum = 4.2;
+
+        wnum++; //this can be add 1 in already stored variable.
+
+        num--; //this can be remove 1 in already stored variable.
+
+        num1 += 30; //this can be add "30" in already stored variable.
+
+        cout << wnum << endl;
+
+        cout << num << endl;
+
+        cout << num1;
+}
