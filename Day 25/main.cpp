25c2d0c143379edc563c34a8de3c5198b7b40a59
Satish Mane <manes1777@gmail.com>
2023-06-01 05:21:07 +0530

    Create main.cpp

---
+++ Day 25/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    if(n>0){
+        cout<<"A is Positive ";
+    }
+    else{
+        cout<<"it is negative ";
+    }
+    return 0;
+}
