5e3b2749640f0a1d36abae2e1eff3932ae6f7229
Satish Mane <manes1777@gmail.com>
2023-06-01 05:21:05 +0530

    Create greater.cpp

---
+++ Day 25/greater.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter  the value of A ";
+    cin>>a;
+
+    cout<<"Enter the value of B ";
+    cin>>b;
+    if(a>b){
+        cout<<" A is Greate than b ";
+    }
+
+    if(b>a){
+        cout<<" B is Greater than A ";
+    }
+
+    else{
+        cout<<"they are same";
+    }
+    
+}
